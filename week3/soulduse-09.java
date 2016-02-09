8c166511f4ca66977d868779664254314b6e157d
soulduse <soulduse@naver.com>
2016-02-09 16:19:37 +0900

    01~19번 기초문제 코딩완료. 본문제는 못품.

---
+++ week3/soulduse-09.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numN = sc.nextInt();
+
+        for(int i=numN; i>0; i--)
+            System.out.println(i);
+    }
+}
